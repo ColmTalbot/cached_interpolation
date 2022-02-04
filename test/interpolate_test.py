f6b4f3a3b6310a87bc89012b4ed4f83191d6339a
Colm Talbot <colm.talbot@ligo.org>
2022-02-04 08:01:57 -0800

    fix build update

--- test/interpolate_test.py
+++ test/interpolate_test.py
@@ -16,20 +16,33 @@ class SplineTest(unittest.TestCase):
 
     def test_cubic_matches_scipy(self):
         spl = CachingInterpolant(self.x_values, self.y_values, kind="cubic")
-        scs = CubicSpline(x=self.x_values, y=self.y_values, bc_type="natural")
         test_points = np.random.uniform(0, 1, 10000)
-        diffs = spl(test_points) - scs(test_points)
-        self.assertLess(np.max(diffs), 1e-10)
+        max_diff = 0
+        for _ in range(100):
+            y_values = np.random.uniform(-1, 1, 10)
+            scs = CubicSpline(x=self.x_values, y=y_values, bc_type="natural")
+            diffs = spl(test_points, y=y_values) - scs(test_points)
+            max_diff = max(np.max(diffs), max_diff)
+        self.assertLess(max_diff, 1e-10)
 
     def test_nearest_matches_scipy(self):
         spl = CachingInterpolant(self.x_values, self.y_values, kind="nearest")
-        scs = interp1d(x=self.x_values, y=self.y_values, kind="nearest")
         test_points = np.random.uniform(0, 1, 10000)
-        diffs = spl(test_points) - scs(test_points)
-        self.assertLess(np.max(diffs), 1e-10)
+        max_diff = 0
+        for _ in range(100):
+            y_values = np.random.uniform(-1, 1, 10)
+            scs = interp1d(x=self.x_values, y=y_values, kind="nearest")
+            diffs = spl(test_points, y=y_values) - scs(test_points)
+            max_diff = max(np.max(diffs), max_diff)
+        self.assertLess(max_diff, 1e-10)
 
     def test_linear_matches_numpy(self):
         spl = CachingInterpolant(self.x_values, self.y_values, kind="linear")
         test_points = np.random.uniform(0, 1, 10000)
-        diffs = spl(test_points) - np.interp(test_points, self.x_values, self.y_values)
-        self.assertLess(np.max(diffs), 1e-10)
+        max_diff = 0
+        for _ in range(100):
+            y_values = np.random.uniform(-1, 1, 10)
+            npy = np.interp(test_points, self.x_values, y_values)
+            diffs = spl(test_points, y=y_values) - npy
+            max_diff = max(np.max(diffs), max_diff)
+        self.assertLess(max_diff, 1e-10)
