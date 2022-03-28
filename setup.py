63230db9ee784371641bb93c37a30602ff24e937
Colm Talbot <colm.talbot@ligo.org>
2022-03-28 13:48:04 -0400

    Fix boundaries and allow complex input

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import Extension, setup
-from Cython.Build import cythonize
 
 import numpy as np
 
 extensions = [
-    Extension("cached_interpolate.build", ["cached_interpolate/build.pyx"], include_dirs=[np.get_include()]),
+    Extension(
+        "cached_interpolate.build",
+        ["cached_interpolate/build.pyx"],
+        include_dirs=[np.get_include()],
+    ),
 ]
 setup(
     name="cached_interpolate",
-    ext_modules=cythonize(extensions, language_level="3"),
+    ext_modules=extensions,
     install_requires=["numpy"],
     packages=["cached_interpolate"],
 )
--- test/interpolate_test.py
+++ test/interpolate_test.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest import mock
 
 import numpy as np
 from scipy.interpolate import interp1d, CubicSpline
@@ -33,4 +34,46 @@ class SplineTest(unittest.TestCase):
         spl = CachingInterpolant(self.x_values, self.y_values, kind="linear")
         test_points = np.random.uniform(0, 1, 10000)
         diffs = spl(test_points) - np.interp(test_points, self.x_values, self.y_values)
+        self.assertLess(np.max(diffs), 1e-10)
+
+    def test_single_input(self):
+        spl = CachingInterpolant(self.x_values, self.y_values, kind="linear")
+        self.assertEqual(spl(0), self.y_values[0])
+
+    def test_interpolation_at_lower_bound(self):
+        spl = CachingInterpolant(self.x_values, self.y_values, kind="linear")
+        test_point = 0
+        self.assertEqual(spl(test_point), self.y_values[0])
+
+    def test_interpolation_at_upper_bound(self):
+        spl = CachingInterpolant(self.x_values, self.y_values, kind="linear")
+        test_point = 1
+        self.assertEqual(spl(test_point), self.y_values[-1])
+
+    def test_bad_interpolation_method_raises_error(self):
+        with self.assertRaises(ValueError):
+            _ = CachingInterpolant(self.x_values, self.y_values, kind="bad method")
+
+    def test_running_without_new_y_values(self):
+        spl = CachingInterpolant(self.x_values, self.y_values, kind="linear")
+        with mock.patch.object(spl, "build") as mocked:
+            _ = spl(0, y=np.random.uniform(-1, 1, 10), use_cache=False)
+        mocked.assert_called()
+
+    def test_running_with_complex_input_linear(self):
+        y_values = self.y_values * np.exp(1j * np.random.uniform(0, 2 * np.pi, 10))
+        spl = CachingInterpolant(self.x_values, y_values, kind="linear")
+        scs = interp1d(x=self.x_values, y=y_values, kind="linear")
+        test_points = np.random.uniform(0, 1, 10)
+        scs_test = scs(test_points)
+        diffs = spl(test_points) - scs_test
+        self.assertLess(np.max(diffs), 1e-10)
+
+    def test_running_with_complex_input_cubic(self):
+        y_values = self.y_values * np.exp(1j * np.random.uniform(0, 2 * np.pi, 10))
+        spl = CachingInterpolant(self.x_values, y_values, kind="cubic")
+        scs = CubicSpline(x=self.x_values, y=y_values, bc_type="natural")
+        test_points = np.random.uniform(0, 1, 10)
+        scs_test = scs(test_points)
+        diffs = spl(test_points) - scs_test
         self.assertLess(np.max(diffs), 1e-10)
